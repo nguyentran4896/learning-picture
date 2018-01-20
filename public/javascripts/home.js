f53ccb181c8f7d5dce42b16bce989ef5d6364df5
nguyentran4896 <nguyentran4896@gmail.com>
2018-01-20 11:34:59 +0700

    added upload image to firebase

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,10 +1,10 @@
 firebaseConfig = {
   apiKey: "AIzaSyBaswFYrRy6dl1JJmS21lRmng2363Rc64Q",
-  authDomain: " elpicture-86fb4.firebaseapp.com ",
-  databaseURL: " https://elpicture-86fb4.firebaseio.com ",
-  projectId: " elpicture-86fb4 ",
-  storageBucket: " elpicture-86fb4.appspot.com ",
-  messagingSenderId: "1097886600472 "
+  authDomain: "elpicture-86fb4.firebaseapp.com",
+  databaseURL: "https://elpicture-86fb4.firebaseio.com",
+  projectId: "elpicture-86fb4",
+  storageBucket: "elpicture-86fb4.appspot.com",
+  messagingSenderId: "1097886600472"
 }
 
 
@@ -18,39 +18,76 @@ const app = new Clarifai.App({
 
 function shareController($scope, $window, $firebaseObject, $firebaseArray, $http) {
   firebase.initializeApp(firebaseConfig);
+  var storageRef = firebase.storage().ref();
+  var databaseRef = firebase.database().ref();
 
   $scope.series = [];
 
   $scope.array = $firebaseArray(firebase.database().ref());
 
-  $scope.array.$loaded().then(function () {
-    for (let i = 0; i < $scope.array.length; i++) {
-      $scope.series.push($scope.array[i]);
-    }
-  })
+  // $scope.array.$loaded().then(function () {
+  //   for (let i = 0; i < $scope.array.length; i++) {
+  //     $scope.series.push($scope.array[i]);
+  //   }
+  // })
 
-  $scope.array.$watch(function (event) {
-    $scope.series.push($scope.array[$scope.array.length - 1]);
-  });
+  // $scope.array.$watch(function (event) {
+  //   $scope.series.push($scope.array[$scope.array.length - 1]);
+  // });
 
+  $scope.uploadImage = function (fileInput) {
+    let file = $(fileInput)[0].files[0]
+    var metadata = {
+      contentType: 'image/jpeg',
+    };
+    
+    // Upload the file and metadata
+    var uploadTask = storageRef.child('images/mountains.jpg').put(file, metadata);
+
+    uploadTask.on('state_changed', function(snapshot){
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+      switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+          console.log('Upload is paused');
+          break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+          console.log('Upload is running');
+          break;
+      }
+    }, function(error) { console.log(error) }, function() {
+      var downloadURL = uploadTask.snapshot.downloadURL;
+      predict(downloadURL)
+      $('img').removeClass('hide');
+      $('img').attr('src', downloadURL);
+    });
+  }
 
   $('button').on('click', function () {
-    $('img').removeClass('hide');
-    $('img').attr('src', $('input').val());
-    app.models.predict(Clarifai.GENERAL_MODEL, $('input').val()).then(
-      function (response) {
-        // do something with response
-        console.log(response)
-        let results = response.outputs[0].data.concepts;
-        $('.result div').remove();
-        for (let i = 0; i < results.length; i++) {
-          $('.result').append('<div>' + results[i].name + '</div>')
-        }
-      },
-      function (err) {
-        // there was an error
-        console.log(err)
-      }
-    );
+    let url = $('#input').val()
+    if(url){
+      predict($('#input').val())
+      $('img').removeClass('hide');
+      $('img').attr('src', url);
+    }
   })
+}
+
+function predict (imageURL) {
+  if(!imageURL) return;
+  app.models.predict(Clarifai.GENERAL_MODEL, imageURL).then(
+    function (response) {
+      // do something with response
+      console.log(response)
+      let results = response.outputs[0].data.concepts;
+      $('.result div').remove();
+      for (let i = 0; i < results.length; i++) {
+        $('.result').append('<div>' + results[i].name + '</div>')
+      }
+    },
+    function (err) {
+      // there was an error
+      console.log(err)
+    }
+  );
 }
