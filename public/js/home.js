073a4441d3057d8c31f3ed218026c51ed494da28
nguyentran4896 <nguyentran4896@gmail.com>
2018-01-20 16:55:44 +0700

    fixed javascripts errors

--- public/js/home.js
+++ public/js/home.js
@@ -107,7 +107,23 @@ function shareController($scope, $window, $firebaseObject, $firebaseArray, $http
           $scope.$apply()
         })
       },
-      function (err) { console.log(err) }
+      function (err) { 
+        console.log(err)
+      
+        let results = [{"id":"ai_9c0Hmcx0","name":"exhibition","value":0.9803946,"app_id":"main"},{"id":"ai_l8TKp2h5","name":"people","value":0.97780854,"app_id":"main"},{"id":"ai_WBQfVV0p","name":"city","value":0.95966643,"app_id":"main"},{"id":"ai_7WNVdPhm","name":"competition","value":0.95694584,"app_id":"main"},{"id":"ai_6lhccv44","name":"business","value":0.9566424,"app_id":"main"},{"id":"ai_TJ9wFfK5","name":"portrait","value":0.95418936,"app_id":"main"},{"id":"ai_p9bzR7fH","name":"education","value":0.95136654,"app_id":"main"},{"id":"ai_VPmHr5bm","name":"adult","value":0.94742644,"app_id":"main"},{"id":"ai_rRDgzFQs","name":"school","value":0.94527024,"app_id":"main"},{"id":"ai_dxSG2s86","name":"man","value":0.94030625,"app_id":"main"},{"id":"ai_H6MK6dlj","name":"commerce","value":0.93880785,"app_id":"main"},{"id":"ai_13NdwKqz","name":"festival","value":0.9204707,"app_id":"main"},{"id":"ai_ggQlMG6W","name":"industry","value":0.91724026,"app_id":"main"},{"id":"ai_86sS08Pw","name":"wear","value":0.9166343,"app_id":"main"},{"id":"ai_dngMN46t","name":"fashion","value":0.9148294,"app_id":"main"},{"id":"ai_RQccV41p","name":"woman","value":0.90841854,"app_id":"main"},{"id":"ai_vTkXGCW9","name":"International","value":0.9045516,"app_id":"main"},{"id":"ai_G8PC3qNc","name":"university","value":0.8939265,"app_id":"main"},{"id":"ai_bmls4LpL","name":"group","value":0.8899503,"app_id":"main"},{"id":"ai_S1qBKn3x","name":"stock","value":0.88925576,"app_id":"main"}];
+        $('.result div').remove();
+        for (let i = 0; i < results.length; i++) {
+          $('.result').append('<div>' + results[i].name + '</div>')
+        }
+        $.post('/translate-arr', {strArr: JSON.stringify(results.map(x=>x.name).splice(0,10))}, function(res){
+          $scope.cardArr = res.arr
+          console.log($scope.cardArr)
+
+          $scope.isChoosingImage = false
+          $scope.$apply()
+        })
+      
+      }
     );
   }
   
