0a3772b08677dfe26bd500082614069c1832e686
nguyentran4896 <nguyentran4896@gmail.com>
2018-01-20 19:21:00 +0700

    added common file

---
+++ public/js/common.js
@@ -0,0 +1,27 @@
+var COMMON = {
+    setCookie: function (cname, cvalue, exdays) {
+        if (exdays == 0) {
+            document.cookie = cname + '=' + cvalue + ';path=/';
+        } else {
+            var d = new Date();
+            d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+            var expires = 'expires=' + d.toUTCString();
+            document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
+        }
+    },
+    getCookie: function (cname) {
+        var name = cname + '=';
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return '';
+    }    
+}
