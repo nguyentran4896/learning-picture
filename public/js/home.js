b0c426d68644fd3a023a733392242b462d4ffa6f
Iker <tdtrinh.web@gmail.com>
2018-01-21 00:41:14 +0700

    update progress bar

--- public/js/home.js
+++ public/js/home.js
@@ -29,7 +29,7 @@ function homeController($scope, $window, $firebaseObject, $firebaseArray, $http)
     onNewFile: function(id, file){
       uploadFileToFirebase(file)
     }
-  });
+  })
 
 
   $scope.uploadImage = function (fileInput) {
@@ -47,6 +47,21 @@ function homeController($scope, $window, $firebaseObject, $firebaseArray, $http)
 
     uploadTask.on('state_changed', function(snapshot){
       var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      var bar = $('#drop-area').find('div.progress-bar');
+
+      bar.width(progress + '%').attr('aria-valuenow', progress);
+
+      if (progress === 0){
+        bar.html('');
+      } else {
+        bar.html(parseInt(progress) + '%');
+      }
+
+      if (progress == 100){
+        $scope.isChoosingImage = false
+        $scope.$apply()
+      }
+
       console.log('Upload is ' + progress + '% done');
       switch (snapshot.state) {
         case firebase.storage.TaskState.PAUSED: // or 'paused'
@@ -58,7 +73,6 @@ function homeController($scope, $window, $firebaseObject, $firebaseArray, $http)
       }
     }, function(error) { console.log(error) }, function() {
       var downloadURL = uploadTask.snapshot.downloadURL;
-      $scope.isChoosingImage = false
       $scope.$apply()
       predict(downloadURL)
       $('.review-image img').removeClass('hide');
@@ -99,8 +113,6 @@ function homeController($scope, $window, $firebaseObject, $firebaseArray, $http)
         })
       },
       function (err) { 
-        console.log(err)
-      
         let results = [{"id":"ai_9c0Hmcx0","name":"exhibition","value":0.9803946,"app_id":"main"},{"id":"ai_l8TKp2h5","name":"people","value":0.97780854,"app_id":"main"},{"id":"ai_WBQfVV0p","name":"city","value":0.95966643,"app_id":"main"},{"id":"ai_7WNVdPhm","name":"competition","value":0.95694584,"app_id":"main"},{"id":"ai_6lhccv44","name":"business","value":0.9566424,"app_id":"main"},{"id":"ai_TJ9wFfK5","name":"portrait","value":0.95418936,"app_id":"main"},{"id":"ai_p9bzR7fH","name":"education","value":0.95136654,"app_id":"main"},{"id":"ai_VPmHr5bm","name":"adult","value":0.94742644,"app_id":"main"},{"id":"ai_rRDgzFQs","name":"school","value":0.94527024,"app_id":"main"},{"id":"ai_dxSG2s86","name":"man","value":0.94030625,"app_id":"main"},{"id":"ai_H6MK6dlj","name":"commerce","value":0.93880785,"app_id":"main"},{"id":"ai_13NdwKqz","name":"festival","value":0.9204707,"app_id":"main"},{"id":"ai_ggQlMG6W","name":"industry","value":0.91724026,"app_id":"main"},{"id":"ai_86sS08Pw","name":"wear","value":0.9166343,"app_id":"main"},{"id":"ai_dngMN46t","name":"fashion","value":0.9148294,"app_id":"main"},{"id":"ai_RQccV41p","name":"woman","value":0.90841854,"app_id":"main"},{"id":"ai_vTkXGCW9","name":"International","value":0.9045516,"app_id":"main"},{"id":"ai_G8PC3qNc","name":"university","value":0.8939265,"app_id":"main"},{"id":"ai_bmls4LpL","name":"group","value":0.8899503,"app_id":"main"},{"id":"ai_S1qBKn3x","name":"stock","value":0.88925576,"app_id":"main"}];
         $('.result div').remove();
         for (let i = 0; i < results.length; i++) {
