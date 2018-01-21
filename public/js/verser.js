b2da6344b1c2dbe747636b2d33e5a672786da61e
nguyentran4896 <nguyentran4896@gmail.com>
2018-01-21 10:23:11 +0700

    fixed api

--- public/js/verser.js
+++ public/js/verser.js
@@ -12,8 +12,8 @@ var engApp = angular
   .controller('shareController', ['$scope', '$window', '$firebaseObject', '$firebaseArray', '$http', 'Message', shareController]);
 
 const app = new Clarifai.App({
-  apiKey: 'e59b033ef81c47de963a93e9cb7e101a' //Key Nguyen
-  //apiKey: 'ec1d1ec98ec3403fb91c39bcb7a93f49' //Key luc
+  // apiKey: 'e59b033ef81c47de963a93e9cb7e101a' //Key Nguyen
+  apiKey: 'ec1d1ec98ec3403fb91c39bcb7a93f49' //Key luc
 });
 
 firebase.initializeApp(firebaseConfig);
